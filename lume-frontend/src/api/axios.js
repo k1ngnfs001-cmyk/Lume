d40998a2fdfe7fb7103ad412accb2460608a0240
k1ngnfs <mmamurjon017@gmail.com>
2026-08-13 18:44:43 +0500

    fix axios url and interceptor

--- lume-frontend/src/api/axios.js
+++ lume-frontend/src/api/axios.js
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api',
-});
-
-const axiosInstance = axios.create({
   baseURL: 'https://lume-5mof.onrender.com', // Жёстко прописываем ссылку на бэкенд
   headers: { 'Content-Type': 'application/json' },
 });
+
+// ОБЯЗАТЕЛЬНО: Интерцептор для подстановки JWT токена
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('lumeToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
